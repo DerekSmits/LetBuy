a083ca6cb6b7099ab76a07ac154fddd05c711c33
Derek_Smits <emrb.2708@outlook.com>
2021-05-02 00:51:59 +0300

    Added models

---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetBuy.Models
+{
+    public class Book
+    {
+        public int id { set; get; }
+        public int price { set; get; }
+        public string img { set; get; }
+        public string name { set; get; }
+        public string longDesc { set; get; }
+        public string shortDesc { set; get; }
+        public bool isAvailable { set; get; }
+        public bool isFavourite { set; get; }
+        public virtual Category category { set; get; }
+    }
+}
